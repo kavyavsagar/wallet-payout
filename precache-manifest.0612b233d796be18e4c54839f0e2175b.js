2c63ea12281cd580446c8d0f7db7f391534d467a
Kavya Sagar <kavya_v_s@yahoo.co.in>
2020-03-16 07:52:46 +0400

    Updates

---
+++ precache-manifest.0612b233d796be18e4c54839f0e2175b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07e647a03207dad2c2a4bce6c6b3f4d6",
+    "url": "/wallet-payout/index.html"
+  },
+  {
+    "revision": "5257c362db0e33222fac",
+    "url": "/wallet-payout/static/css/main.62bd52e1.chunk.css"
+  },
+  {
+    "revision": "4964b82caba3956d4cdb",
+    "url": "/wallet-payout/static/js/2.77b220e4.chunk.js"
+  },
+  {
+    "revision": "12da75f7e58616280abf57fe51d0179b",
+    "url": "/wallet-payout/static/js/2.77b220e4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5257c362db0e33222fac",
+    "url": "/wallet-payout/static/js/main.5821d293.chunk.js"
+  },
+  {
+    "revision": "0f76c0e40333f508503d",
+    "url": "/wallet-payout/static/js/runtime-main.76536b1d.js"
+  }
+]);
